fff25f0822bc40627b7a2779f481c7176611430f
zanjs <root@zanjs.com>
2016-06-06 09:41:42 +0800

    v0.0.4 banner :neutral_face:

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,38 @@
 
 
-import gulp from 'gulp';
+import gulp         from 'gulp';
+import uglify       from 'gulp-uglify';
+import rename       from 'gulp-rename';
+import notify       from 'gulp-notify';
+import browserSync  from 'browser-sync';
+import saveLicense  from 'uglify-save-license';
+import header       from 'gulp-header';
 
-import uglify from 'gulp-uglify';
-import rename from 'gulp-rename';
-import notify from 'gulp-notify';
-import browserSync from 'browser-sync';
-import saveLicense from 'uglify-save-license';
+import pkg          from './package.json';
 
 const  reload  = browserSync.create().reload;
 
+
+const banner = [
+  '/*! ',
+    '<%= pkg.name %> ',
+    'v<%= pkg.version %> | ',
+    `(c) ${new Date()} <%= pkg.author %> |`,
+    ' <%= pkg.homepage %>',
+  ' */',
+  '\n'
+].join('');
+
+
+
 gulp.task('scripts', () => {
   gulp.src('./src/datashow.js')
-    .pipe(uglify({
-      mangle: false,
-      preserveComments: saveLicense
-    }))
+    // .pipe(uglify({
+    //   mangle: false,
+    //   preserveComments: saveLicense
+    // }))
+    .pipe(uglify())
+    .pipe(header(banner, { pkg }))
     .pipe(rename('datashow.js'))
     .pipe(gulp.dest('./dist/'))
     .pipe(reload({stream: true}))
@@ -30,7 +47,7 @@ gulp.task('watch', () => {
     });
     
     // 看守所有.js档
-    gulp.watch('./src/js/*.js', ['scripts']);
+    gulp.watch('./src/*.js', ['scripts']);
     gulp.watch('./*.html',['scripts']);
     
 });
